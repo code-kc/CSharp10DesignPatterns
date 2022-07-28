81f34f4da3dffbfa839d93155a2e30895c5e05bc
Avinash K C <avinash.kc@sasken.com>
2022-07-28 18:51:00 +0530

    Singleton

--- Starter solution/Singleton/Implementation.cs
+++ Starter solution/Singleton/Implementation.cs
@@ -1,4 +1,30 @@
-﻿namespace Singleton
+﻿namespace Singleton;
+
+public class Logger
 {
-  
+    private static readonly Lazy<Logger> _lazyLogger = new Lazy<Logger>(() => new Logger());
+    // private static Logger? _instance; 
+    public static Logger Instance
+    {
+        get 
+        {
+            return _lazyLogger.Value;
+            // if(_instance == null)
+            // {
+            //     _instance = new Logger();
+            // }
+            // return _instance;
+        }
+    }
+
+    protected Logger()
+    {
+
+    }
+
+    public void Log(string message)
+    {
+        Console.WriteLine($"Message to log: {message}");
+    }
+    
 }
